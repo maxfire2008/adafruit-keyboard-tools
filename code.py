8e6dedf4147dd8dac9d95418cdd300a99a2c0a6b
maxrichardburgess <22722054+maxfire2008@users.noreply.github.com>
2021-10-03 18:25:43 +1100

    Update code.py

--- code.py
+++ code.py
@@ -3,6 +3,7 @@ import time
 import board
 import neopixel
 import digitalio
+import random
 
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=0.05, auto_write=False)
 
@@ -13,30 +14,44 @@ from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 kbd = Keyboard(usb_hid.devices)
 layout = KeyboardLayoutUS(kbd)
 
+STACKABLES_WAIT = 601
+
 _discord_dank_memer_wait_times = {
     "pls beg": 46,
     "pls hunt": 41,
     "pls fish": 41,
     "pls dig": 41,
-    "pls dep all": 301,
+    "pls dep all": STACKABLES_WAIT,
+    "pls sell boar all": STACKABLES_WAIT,
+    "pls sell cookie all": STACKABLES_WAIT,
+    "pls sell dear all": STACKABLES_WAIT,    
+    "pls sell duck all": STACKABLES_WAIT,
+    "pls sell fish all": STACKABLES_WAIT,
+    "pls sell junk all": STACKABLES_WAIT,
+    "pls sell rabbit all": STACKABLES_WAIT,
+    "pls sell seaweed all": STACKABLES_WAIT,
+    "pls sell skunk all": STACKABLES_WAIT,
+    "pls sell work all": STACKABLES_WAIT,
 }
-_discord_dank_memer_prev_action = {
-    "pls beg": -3600,
-    "pls hunt": -3600,
-    "pls fish": -3600,
-    "pls dig": -3600,
-    "pls dep all": -3600,
-}
+_discord_dank_memer_wait_times_randomised = _discord_dank_memer_wait_times
+_discord_dank_memer_prev_action = {}
 
 def discord_dank_memer():
     global _discord_dank_memer_wait_times
+    global _discord_dank_memer_wait_times_randomised
     global _discord_dank_memer_prev_action
     time_int = int(time.time())
     for action in _discord_dank_memer_wait_times:
-        if _discord_dank_memer_prev_action[action]+_discord_dank_memer_wait_times[action] < time_int and max(_discord_dank_memer_prev_action.values())+2 < time_int:
+        if action not in _discord_dank_memer_prev_action:
+            _discord_dank_memer_prev_action[action] = random.randint(int(time.time()-30),int(time.time()+30))
+        if _discord_dank_memer_prev_action[action]+_discord_dank_memer_wait_times_randomised[action] < time_int and max(_discord_dank_memer_prev_action.values())+(random.randint(1000,3000)/1000) < time_int:
+            print(action)
             layout.write(action)
             kbd.send(Keycode.ENTER)
             _discord_dank_memer_prev_action[action] = time_int
+            _discord_dank_memer_wait_times_randomised[action] = _discord_dank_memer_wait_times[action]+(random.randint(2000,8000)/1000)
+    if time_int % 5 == 0:
+        print(_discord_dank_memer_wait_times_randomised)
 
 def test_print():
     print("app run")
